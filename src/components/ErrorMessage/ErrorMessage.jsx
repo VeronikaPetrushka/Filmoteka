5e2634ba4a82f9d2c849591c84fbec18ac16db3a
Veronika <veronikapetrushka@gmail.com>
2024-04-02 20:21:41 +0200

    base

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,7 +0,0 @@
-const ErrorMessage = () => {
-    return (
-        <p style={{fontSize: 20, color: "black"}}>Whooops...Something went wrong, please reloade the page!</p>
-    );
-};
-
-export default ErrorMessage;
